5dbb35f0427320461bb8a02a6333c8953b8373aa
Artiom Chilaru <artiom@flexlabs.org>
2018-02-19 23:19:35 +0000

    + Adding CI builds + Adding template MySql generator + Adding HOLDLOCK for SqlServer

--- src/FlexLabs.EntityFrameworkCore.Upsert/Generators/MySqlUpsertSqlGenerator.cs
+++ src/FlexLabs.EntityFrameworkCore.Upsert/Generators/MySqlUpsertSqlGenerator.cs
@@ -7,23 +7,18 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace FlexLabs.EntityFrameworkCore.Upsert.Generators
 {
-    public class SqlServerUpsertSqlGenerator : IUpsertSqlGenerator
+    public class MySqlUpsertSqlGenerator : IUpsertSqlGenerator
     {
         public string GenerateCommand(IEntityType entityType, ICollection<string> insertColumns, ICollection<string> joinColumns, ICollection<string> updateColumns, List<(string ColumnName, KnownExpressions Value)> updateExpressions)
         {
             var result = new StringBuilder();
-            result.Append($"MERGE INTO {entityType.Relational().Schema ?? "dbo"}.[{entityType.Relational().TableName}] WITH (HOLDLOCK) AS [T] USING ( VALUES (");
-            result.AppendJoin(", ", insertColumns.Select((v, i) => $"@p{i}"));
-            result.Append($") ) AS [S] (");
-            result.AppendJoin(", ", insertColumns.Select(c => $"[{c}]"));
-            result.Append(") ON ");
-            result.AppendJoin(" AND ", joinColumns.Select(c => $"[T].[{c}] = [S].[{c}]"));
-            result.Append(" WHEN NOT MATCHED BY TARGET THEN INSERT (");
-            result.AppendJoin(", ", insertColumns.Select(c => $"[{c}]"));
+            var schema = entityType.Relational().Schema;
+            result.Append($"INSERT INTO {(schema != null ? schema + "." : null)}{entityType.Relational().TableName} (");
+            result.AppendJoin(", ", insertColumns.Select(c => $"{c}"));
             result.Append(") VALUES (");
-            result.AppendJoin(", ", insertColumns.Select(c => $"[{c}]"));
-            result.Append(") WHEN MATCHED THEN UPDATE SET ");
-            result.AppendJoin(", ", updateColumns.Select((c, i) => $"[{c}] = @p{i + insertColumns.Count}"));
+            result.AppendJoin(", ", insertColumns.Select((v, i) => $"@p{i}"));
+            result.Append(") ON DUPLICATE KEY UPDATE ");
+            result.AppendJoin(", ", updateColumns.Select((c, i) => $"{c} = @p{i + insertColumns.Count}"));
             if (updateExpressions.Count > 0)
             {
                 if (updateColumns.Count > 0)
@@ -31,7 +26,6 @@ namespace FlexLabs.EntityFrameworkCore.Upsert.Generators
                 var argumentOffset = insertColumns.Count + updateColumns.Count;
                 result.AppendJoin(", ", updateExpressions.Select((e, i) => ExpandExpression(i + argumentOffset, e.ColumnName, e.Value)));
             }
-            result.Append(";");
             return result.ToString();
         }
 
@@ -40,13 +34,13 @@ namespace FlexLabs.EntityFrameworkCore.Upsert.Generators
             switch (expression.ExpressionType)
             {
                 case System.Linq.Expressions.ExpressionType.Add:
-                    return $"[{columnName}] = [T].[{columnName}] +  @p{argumentIndex}";
+                    return $"{columnName} = {columnName} + @p{argumentIndex}";
                 case System.Linq.Expressions.ExpressionType.Subtract:
-                    return $"[{columnName}] = [T].[{columnName}] -  @p{argumentIndex}";
+                    return $"{columnName} = {columnName} - @p{argumentIndex}";
                 default: throw new NotSupportedException("Don't know how to process operation: " + expression.ExpressionType);
             }
         }
 
-        public bool Supports(string name) => name == "Microsoft.EntityFrameworkCore.SqlServer";
+        public bool Supports(string name) => name == "MySql.Data.EntityFrameworkCore" || name == "Pomelo.EntityFrameworkCore.MySql";
     }
 }
