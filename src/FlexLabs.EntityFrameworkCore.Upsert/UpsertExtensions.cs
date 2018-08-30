315f47dff039282222ec7aed463bc937a4a61aae
Artiom Chilaru <artiom@flexlabs.org>
2018-08-30 19:32:18 +0100

    * Attempting to fix the service registration * Moving the upsert extensions to the Microsoft.EntityFrameworkCore namespace + Adding new upsert extensions for the DbSet classes

--- src/FlexLabs.EntityFrameworkCore.Upsert/UpsertExtensions.cs
+++ src/FlexLabs.EntityFrameworkCore.Upsert/UpsertExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FlexLabs.EntityFrameworkCore.Upsert;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Internal;
 
-namespace FlexLabs.EntityFrameworkCore.Upsert
+namespace Microsoft.EntityFrameworkCore
 {
     /// <summary>
     /// Extension methods that provide access to upsert commands on a DbContext
@@ -31,6 +33,34 @@ namespace FlexLabs.EntityFrameworkCore.Upsert
             where TEntity : class
         {
             return new UpsertCommandBuilder<TEntity>(dbContext, entities);
+        }
+
+        /// <summary>
+        /// Attempt to insert an entity to the database, or update it if one already exists
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity being upserted</typeparam>
+        /// <param name="dbSet">The db set where the item will be upserted</param>
+        /// <param name="entity">The entity that is being upserted</param>
+        /// <returns>The upsert command builder that is used to configure and run the upsert operation</returns>
+        public static UpsertCommandBuilder<TEntity> Upsert<TEntity>(this DbSet<TEntity> dbSet, TEntity entity)
+            where TEntity : class
+        {
+            var dbContext = dbSet.GetService<ICurrentDbContext>().Context;
+            return Upsert(dbContext, entity);
+        }
+
+        /// <summary>
+        /// Attempt to insert an array of entities to the database, or update them if they already exist
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity being upserted</typeparam>
+        /// <param name="dbSet">The db set where the items will be upserted</param>
+        /// <param name="entities">The entities that are being upserted</param>
+        /// <returns>The upsert command builder that is used to configure and run the upsert operation</returns>
+        public static UpsertCommandBuilder<TEntity> UpsertRange<TEntity>(this DbSet<TEntity> dbSet, params TEntity[] entities)
+            where TEntity : class
+        {
+            var dbContext = dbSet.GetService<ICurrentDbContext>().Context;
+            return UpsertRange(dbContext, entities);
         }
     }
 }
--- src/FlexLabs.EntityFrameworkCore.Upsert/UpsertServiceExtensions.cs
+++ src/FlexLabs.EntityFrameworkCore.Upsert/UpsertServiceExtensions.cs
@@ -1,14 +1,40 @@
 ﻿using FlexLabs.EntityFrameworkCore.Upsert.Runners;
+using Microsoft.EntityFrameworkCore;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class UpsertServiceExtensions
     {
-        public static IServiceCollection AddUpsertCommandGenerator<TGenerator>(this IServiceCollection services)
-            where TGenerator : class, IUpsertCommandRunner
+        /// <summary>
+        /// Register a custom upsert command runner, to replace the built-in ones
+        /// This method can only be used when the service provider is being built externally and passed to Microsoft.EntityFrameworkCore.DbContextOptionsBuilder
+        /// </summary>
+        /// <typeparam name="TRunner">Type of the upsert command runner class</typeparam>
+        /// <param name="services">Service collection where the runner should be registered</param>
+        /// <returns>The service collection passed to this call</returns>
+        public static IServiceCollection ReplaceUpsertCommandRunner<TRunner>(this IServiceCollection services)
+            where TRunner : class, IUpsertCommandRunner
         {
-            services.AddScoped<IUpsertCommandRunner, TGenerator>();
+            services.AddScoped<IUpsertCommandRunner, TRunner>();
             return services;
+        }
+
+        /// <summary>
+        /// Register a custom upsert command runner, to replace the built-in ones
+        /// This method can only be used when EF is building and managing its internal service
+        /// provider. If the service provider is being built externally and passed to Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseInternalServiceProvider(System.IServiceProvider),
+        /// then replacement services should be configured on that service provider before
+        /// it is passed to EF.
+        /// The replacement service gets the same scope as the EF service that it is replacing.
+        /// </summary>
+        /// <typeparam name="TRunner">Type of the upsert command runner class</typeparam>
+        /// <param name="builder">The DbContextOptionsBuilder that is used to configure  the DbContext</param>
+        /// <returns>The DbContextOptionsBuilder passed to this call</returns>
+        public static DbContextOptionsBuilder ReplaceUpsertCommandRunner<TRunner>(this DbContextOptionsBuilder builder)
+            where TRunner : class, IUpsertCommandRunner
+        {
+            builder.ReplaceService<IUpsertCommandRunner, TRunner>();
+            return builder;
         }
     }
 }
