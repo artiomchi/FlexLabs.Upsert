4c2934fee0a807313a60828ecf529a6b6fcfe0d6
Anthony Iacono <anthony.iacono@willowtreeapps.com>
2019-02-04 11:34:20 -0500

    Improve SqlServerUpsertCommandRunner to allow for null values inside matchers

--- test/FlexLabs.EntityFrameworkCore.Upsert.Tests/Runners/SqlServerUpsertCommandRunnerTests.cs
+++ test/FlexLabs.EntityFrameworkCore.Upsert.Tests/Runners/SqlServerUpsertCommandRunnerTests.cs
@@ -9,41 +9,41 @@ namespace FlexLabs.EntityFrameworkCore.Upsert.Tests.Runners
         protected override string NoUpdate_Sql =>
             "MERGE INTO myTable WITH (HOLDLOCK) AS [T] " +
             "USING ( VALUES (@p0, @p1) ) AS [S] ([Name], [Status]) " +
-            "ON [T].[ID] = [S].[ID] " +
+            "ON (([S].[ID] IS NOT NULL AND [T].[ID] = [S].[ID]) OR ([S].[ID] IS NULL AND [T].[ID] IS NULL)) " +
             "WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], [Status]) VALUES ([Name], [Status]);";
 
         protected override string Update_Constant_Sql =>
             "MERGE INTO myTable WITH (HOLDLOCK) AS [T] " +
             "USING ( VALUES (@p0, @p1) ) AS [S] ([Name], [Status]) " +
-            "ON [T].[ID] = [S].[ID] " +
+            "ON (([S].[ID] IS NOT NULL AND [T].[ID] = [S].[ID]) OR ([S].[ID] IS NULL AND [T].[ID] IS NULL)) " +
             "WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], [Status]) VALUES ([Name], [Status]) " +
             "WHEN MATCHED THEN UPDATE SET [Name] = @p2;";
 
         protected override string Update_Source_Sql =>
             "MERGE INTO myTable WITH (HOLDLOCK) AS [T] " +
             "USING ( VALUES (@p0, @p1) ) AS [S] ([Name], [Status]) " +
-            "ON [T].[ID] = [S].[ID] " +
+            "ON (([S].[ID] IS NOT NULL AND [T].[ID] = [S].[ID]) OR ([S].[ID] IS NULL AND [T].[ID] IS NULL)) " +
             "WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], [Status]) VALUES ([Name], [Status]) " +
             "WHEN MATCHED THEN UPDATE SET [Name] = [S].[Name];";
 
         protected override string Update_Source_RenamedCol_Sql =>
             "MERGE INTO myTable WITH (HOLDLOCK) AS [T] " +
             "USING ( VALUES (@p0, @p1) ) AS [S] ([Name], [Status]) " +
-            "ON [T].[ID] = [S].[ID] " +
+            "ON (([S].[ID] IS NOT NULL AND [T].[ID] = [S].[ID]) OR ([S].[ID] IS NULL AND [T].[ID] IS NULL)) " +
             "WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], [Status]) VALUES ([Name], [Status]) " +
             "WHEN MATCHED THEN UPDATE SET [Name] = [S].[Name2];";
 
         protected override string Update_BinaryAdd_Sql =>
             "MERGE INTO myTable WITH (HOLDLOCK) AS [T] " +
             "USING ( VALUES (@p0, @p1) ) AS [S] ([Name], [Status]) " +
-            "ON [T].[ID] = [S].[ID] " +
+            "ON (([S].[ID] IS NOT NULL AND [T].[ID] = [S].[ID]) OR ([S].[ID] IS NULL AND [T].[ID] IS NULL)) " +
             "WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], [Status]) VALUES ([Name], [Status]) " +
             "WHEN MATCHED THEN UPDATE SET [Status] = [T].[Status] + @p2;";
 
         protected override string Update_Coalesce_Sql =>
             "MERGE INTO myTable WITH (HOLDLOCK) AS [T] " +
             "USING ( VALUES (@p0, @p1) ) AS [S] ([Name], [Status]) " +
-            "ON [T].[ID] = [S].[ID] " +
+            "ON (([S].[ID] IS NOT NULL AND [T].[ID] = [S].[ID]) OR ([S].[ID] IS NULL AND [T].[ID] IS NULL)) " +
             "WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], [Status]) VALUES ([Name], [Status]) " +
             "WHEN MATCHED THEN UPDATE SET [Status] = COALESCE([T].[Status], @p2);";
     }
