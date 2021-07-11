7c2644681fbcb93d94e0a8da20924ca5bb3172af
Artiom Chilaru <artiom@flexlabs.org>
2021-07-11 17:07:03 +0100

    Adding pre-release deployment batch parameter

--- build.release.signed.bat
+++ build.release.signed.bat
@@ -1,11 +1,17 @@
 @IF [%1] == [] GOTO NOPARAM
 
+SET VERSION=%1
+@IF NOT [%2] == [] (
+    SET SUFFIX=--version-suffix %2
+    SET VERSION=%1-%2
+)
+
 @ECHO -- Building solution in release mode
-dotnet pack -c Release -p:SignCertificateName="Open Source Developer%%2c Artiom Chilaru"
+dotnet pack -c Release -p:SignCertificateName="Open Source Developer%%2c Artiom Chilaru" %SUFFIX%
 @IF ERRORLEVEL 1 goto ERROR
 
 @ECHO -- Signing the nuget package
-nuget sign src\FlexLabs.EntityFrameworkCore.Upsert\bin\Release\FlexLabs.EntityFrameworkCore.Upsert.%1.nupkg -CertificateSubjectName "Open Source Developer, Artiom Chilaru" -Timestamper http://timestamp.digicert.com
+nuget sign src\FlexLabs.EntityFrameworkCore.Upsert\bin\Release\FlexLabs.EntityFrameworkCore.Upsert.%VERSION%.nupkg -CertificateSubjectName "Open Source Developer, Artiom Chilaru" -Timestamper http://timestamp.digicert.com
 @IF ERRORLEVEL 1 goto ERROR
 
 @EXIT /B %errorlevel%
