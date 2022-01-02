e80775eaababaecdc7313623ae954e0521fb8604
Artiom Chilaru <artiom@flexlabs.org>
2022-01-02 16:10:34 +0000

    Patching the DateTime handling for PostgreSQL

--- test/FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests/Base/TestDbContext.cs
+++ test/FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests/Base/TestDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -40,13 +39,6 @@ namespace FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests.Base
             {
                 modelBuilder.Entity<JsonData>().Property(j => j.Data).HasColumnType("jsonb");
                 modelBuilder.Entity<JsonData>().Property(j => j.Child).HasColumnType("jsonb");
-                foreach (var property in modelBuilder!.Model.GetEntityTypes()
-                             .SelectMany(t => t.GetProperties())
-                             .Where(p => p.ClrType == typeof(DateTime?) || p.ClrType == typeof(DateTime))
-                        )
-                {
-                    property.SetColumnType("timestamp");
-                }
             }
             else
             {
