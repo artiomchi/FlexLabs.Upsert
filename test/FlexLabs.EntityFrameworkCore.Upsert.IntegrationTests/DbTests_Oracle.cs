0dd6b0b7278d7b757a48b96abc30160532b63dd2
Голубев Д.О. <golubev@sicon.ru>
2024-02-07 14:38:25 +0300

    write test runner for oracle (failing at this moment)

---
+++ test/FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests/DbTests_Oracle.cs
@@ -0,0 +1,34 @@
+﻿using DotNet.Testcontainers.Containers;
+using FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests.Base;
+using FlexLabs.EntityFrameworkCore.Upsert.Tests.EF;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.Oracle;
+using Xunit;
+
+namespace FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests
+{
+#if !NOORACLE
+    public class DbTests_Oracle : DbTestsBase, IClassFixture<DbTests_Oracle.DatabaseInitializer>
+    {
+        public sealed class DatabaseInitializer : DatabaseInitializerFixture
+        {
+            public override DbDriver DbDriver => DbDriver.Oracle;
+
+            protected override IContainer BuildContainer()
+                => new OracleBuilder()
+                    .Build();
+
+            protected override void ConfigureContextOptions(DbContextOptionsBuilder<TestDbContext> builder)
+            {
+                var connectionString = (TestContainer as IDatabaseContainer)?.GetConnectionString();
+                builder.UseOracle(connectionString);
+            }
+        }
+
+        public DbTests_Oracle(DatabaseInitializer contexts)
+            : base(contexts)
+        {
+        }
+    }
+#endif
+}
