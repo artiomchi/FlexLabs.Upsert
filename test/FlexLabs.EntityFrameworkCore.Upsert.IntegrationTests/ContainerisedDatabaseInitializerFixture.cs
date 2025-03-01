f30e43cedf2607a10addd20981c04f442f3a872b
Cédric Luthi <cedric.luthi@gmail.com>
2025-03-01 15:40:08 +0100

    Improve integration tests
    * Use Testcontainers.Xunit for better errors in case Docker is not available.
    * Don't reuse containers for the integration tests but create a fresh container each time.
    * Update Oracle to use the `gvenzl/oracle-free:23-slim-faststart` image so that tests can run on Apple ARM MacBooks.
    * Better error if USE_LOCAL_SERVICE is used for tests other that SQL Server.

--- test/FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests/ContainerisedDatabaseInitializerFixture.cs
+++ test/FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests/ContainerisedDatabaseInitializerFixture.cs
@@ -1,41 +1,48 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using Testcontainers.Xunit;
+using Xunit;
 
 namespace FlexLabs.EntityFrameworkCore.Upsert.IntegrationTests
 {
-    public abstract class ContainerisedDatabaseInitializerFixture<TContainer> : DatabaseInitializerFixture
+    public abstract class ContainerisedDatabaseInitializerFixture<TBuilder, TContainer>(DbContainerFixture<TBuilder, TContainer> dbContainerFixture)
+        : DatabaseInitializerFixture, IClassFixture<DbContainerFixture<TBuilder, TContainer>>
+        where TBuilder : IContainerBuilder<TBuilder, TContainer>, new()
         where TContainer : IContainer, IDatabaseContainer
     {
-        public TContainer TestContainer { get; }
+        private static readonly string DbDriverName = typeof(TContainer).Name.Replace("Container", "");
 
-        public ContainerisedDatabaseInitializerFixture()
-        {
-            if (!BuildEnvironment.UseLocalService)
-            {
-                TestContainer = BuildContainer();
-            }
-        }
+        private readonly bool _useContainer = !BuildEnvironment.UseLocalService;
 
-        protected abstract TContainer BuildContainer();
+        protected static TBuilder ConfigureContainer(TBuilder builder)
+            => builder.WithName($"flexlabs_upsert_{DbDriverName.ToLowerInvariant()}");
+
+        protected string ConnectionString
+            => _useContainer ? dbContainerFixture.Container.GetConnectionString() : LocalServiceConnectionString;
+
+        protected virtual string LocalServiceConnectionString
+            => throw new InvalidOperationException($"{DbDriverName} tests don't support the USE_LOCAL_SERVICE environment variable.");
 
         public override async Task InitializeAsync()
         {
-            if (TestContainer is not null)
+            if (_useContainer)
             {
-                await TestContainer.StartAsync();
+                await ((IAsyncLifetime)dbContainerFixture).InitializeAsync();
             }
 
             await base.InitializeAsync();
         }
 
-        // Some containers don't start up properly if they're stopped and started again, so we will leave them running
-        // In CI environments, they will be cleared up automatically, when developing locally - you may need to clean up manually
-        //public override async Task DisposeAsync()
-        //{
-        //    if (TestContainer is not null)
-        //    {
-        //        await TestContainer.StopAsync();
-        //    }
-        //}
+        public override async Task DisposeAsync()
+        {
+            if (_useContainer)
+            {
+                await ((IAsyncLifetime)dbContainerFixture).DisposeAsync();
+            }
+
+            await base.DisposeAsync();
+        }
     }
 }
